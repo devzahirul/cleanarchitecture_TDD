736230b745b16e16c009c0d5f00f206407050248
Zahirul <jislam150@gmail.com>
2023-11-14 13:23:45 +0600

    Initial Commit

---
+++ CleanArchecture/CleanArchectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchectureApp.swift
+//  CleanArchecture
+//
+//  Created by Hologram1 on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
